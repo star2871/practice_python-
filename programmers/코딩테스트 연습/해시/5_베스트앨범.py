77400b4f5da4622f6cbec1c9ef83238fbdb18898
star2871 <star2871@gmail.com>
2022-09-04 21:43:45 +0900

    프로그래머스 해시 베스트 앨범 풀이 완료

---
+++ programmers/코딩테스트 연습/해시/5_베스트앨범.py
@@ -0,0 +1,31 @@
+import sys
+
+sys.stdin = open("5_베스트앨범.txt")
+
+
+genres = input().split()
+plays = list(map(int,input().split()))
+
+def solution(genres, plays):
+    answer = []
+
+    dic1 = {}
+    dic2 = {}
+
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        if g not in dic1:
+            dic1[g] = [(i, p)]
+        else:
+            dic1[g].append((i, p))
+
+        if g not in dic2:
+            dic2[g] = p
+        else:
+            dic2[g] += p
+
+    for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
+        for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
+            answer.append(i)
+
+    return answer
+print(solution(genres, plays))
