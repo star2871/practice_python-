014632c402b7de7d94c73baa8caebc4c7070f8b8
star2871 <star2871@gmail.com>
2022-09-09 23:55:21 +0900

    기초능력향상 딕셔너리 개수세기 문제 딕셔너리로 풀이 완료

--- 기초 능력 향상/딕셔너리/3_개수세기.py
+++ 기초 능력 향상/딕셔너리/3_개수세기.py
@@ -1,23 +1,26 @@
 import sys
 sys.stdin = open("3_개수세기.txt")
+# 딕셔너리 방법
+T = int(input())
+N_list = list(map(int, input().split()))
+k = int(input())
 
-# dict={"a":1, "b":4, "c":1, "d":2, "e":4, "f":2, "g":4, "h":2, "i":3, "j":4, "k":4}
+dictionary = {i : N_list[i] for i in range(len(N_list))}
+
+cnt = 0
+for key, value in dictionary.items():
+    if value == k:
+        cnt+= 1
+print(cnt)
+
+# 다른 방법
 # T = int(input())
 # N_list = list(map(int, input().split()))
 # k = int(input())
 # cnt = 0
-# for key, value in dict.items():
-#     if value == k:
-#         cnt+= 1
-# print(cnt)
 
-T = int(input())
-N_list = list(map(int, input().split()))
-k = int(input())
-cnt = 0
-
-for i in N_list:
-    if i == k:
-        cnt += 1
+# for i in N_list:
+#     if i == k:
+#         cnt += 1
         
-print(cnt)
+# print(cnt)
