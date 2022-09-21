b48e59845397d26cb2105c82c5b5ce7ae1c206da
star2871 <star2871@gmail.com>
2022-09-21 23:21:06 +0900

    코드와트 boj 11508번 풀이완료

---
+++ 코드와트/두번째주/boj_11508.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("boj_11508.txt")
+
+N = int(input())
+milk_list=[]
+cost = 0
+for tc in range(N):
+    milk_list.append(int(input()))
+    # reverse=True 내림차순으로 정렬 할 수 있다.
+milk_list.sort(reverse=True)
+    
+	# 내림차순으로 정렬을 해놨으니, 3번째로 들어오게 되는 값만 제외시키면 정답이 된다.
+    # ex) cost += m[0],m[1],m[3],m[4],m[6],m[7]...
+    # 훨씬 간결한거같다...
+cost = 0
+for tc in range(N):
+    if(tc%3!=2):
+        cost += milk_list[tc]
+print(cost)
