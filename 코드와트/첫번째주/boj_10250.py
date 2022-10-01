da5f05089c59dd447962127e2661c7648338d019
star2871 <star2871@gmail.com>
2022-10-01 23:06:57 +0900

    코드와트 첫번째주 boj 10250 문제풀이완료

--- 코드와트/첫번째주/boj_10250.py
+++ 코드와트/첫번째주/boj_10250.py
@@ -1,3 +1,17 @@
 import sys
-sys.stdin=open("boj_10250.txt")
 
+sys.stdin = open("boj_10250.txt")
+
+
+n = int(input())
+
+for tc in range(n):
+    H, W, N = map(int, input().split())
+    a = N % H  # 층수
+    b = N // H + 1  # 호수
+
+    if N % H == 0:
+        a = H
+        b = N // H
+
+    print(a * 100 + b)
